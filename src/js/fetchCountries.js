142e1326461f61239c8deab41eff16eb531d627e
DaniilaSaenko <daniila.s777@gmail.com>
2022-12-01 15:56:55 +0200

    DZ-10

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,9 +4,7 @@ export default function fetchCountries(name) {
   return fetch(`${BASE_URL}${name}?fields=name,capital,population,flags,languages`).then(response => {
     if (!response.ok) {
       clearInfo();
-      if (response.status === 404) {
         throw new Error(response.status);
-      }
     }
     return response.json();
   });
